f179a063d775922e4410295e00062eb50e5e0c09
EmmAlva <clioemma_28@hotmail.com>
2017-05-26 19:39:53 -0500

    Ready

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,19 +12,19 @@ function initMap() {
           map: map
         });
 
-
-function buscar(){
+function buscar(event){
+  event.preventDefault();
   if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
   }
 }
-var latitud,longitud;
+var latitud,longitud,miUbicacion;
 
 var funcionExito = function(posicion){
   latitud = posicion.coords.latitude;
   longitud = posicion.coords.longitude;
 
-  var miUbicacion = new google.maps.Marker({
+  miUbicacion = new google.maps.Marker({
   position:{lat:latitud, lng:longitud},
   map:map
  
@@ -39,4 +39,53 @@ var funcionError = function(error){
 
 document.getElementById("encuentrame").addEventListener("click",buscar);
 
+var inputPartida = document.getElementById("punto-partida");
+var inputDestino = document.getElementById("punto-destino");
+
+new google.maps.places.Autocomplete(inputPartida);
+new google.maps.places.Autocomplete(inputDestino);
+
+var directionsService = new google.maps.DirectionsService;
+var directionsDisplay = new google.maps.DirectionsRenderer;
+
+var calculateAndDisplayRoute = function(directionsService, directionsDisplay){
+  directionsService.route({
+    origin: inputPartida.value,
+    destination: inputDestino.value,
+    travelMode: 'DRIVING'
+  },function(response, status){
+    if(status === 'OK'){
+
+      var distancia = Number((response.routes[0].legs[0].distance.text.replace(" km","")).replace(",","."));
+
+      tarifa.classList.remove("none");
+
+      var costo = distancia*1.75;
+      if(costo<4){
+         tarifa.innerHTML = "S/. 4";
+       }    
+          tarifa.innerHTML = "S/. " + parseInt(costo);       
+
+  
+
+      directionsDisplay.setDirections(response);
+  
+      if(miUbicacion!== undefined){
+        miUbicacion.setMap(null);
+      }
+      markadorLaboratoria.setMap(null);
+
+   }else{
+      window.alert("No encontramos una ruta");
+      }
+    });
 }
+
+  directionsDisplay.setMap(map);
+
+  var trazarRuta = function(){
+    calculateAndDisplayRoute(directionsService,directionsDisplay);
+  };
+  document.getElementById("trazar-ruta").addEventListener("click",trazarRuta);
+  }
+
